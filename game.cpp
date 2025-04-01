7e621e82618d0dd5069d40434ce4437af44ab502
MattPiet <mattpietrafesa@gmail.com>
2025-04-01 15:19:03 -0400

    hmm lets try again

--- game.cpp
+++ game.cpp
@@ -5,6 +5,5 @@ void game::printStatment() {
 	// edit these lines she said we can use commented lines
 	// Matthew changing this line it will be n3 
 	// same thing here its being weird so im changing this
-	// git is a bit weird to start with but is easy overall
 	
 }
--- game.h
+++ game.h
@@ -9,9 +9,7 @@ public:
 	/*
 	Matthew changing this line it will be n2
 	its being weird so im changing this as well
-	i dont like this assignment
-	im a little excited for our in class presentations
-	are you?
+
 	*/
 	
 };
